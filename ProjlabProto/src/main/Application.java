30727138bc95fcb7db6c48855a78cd80c3226ac4
Koni <koniskolaja@gmail.com>
2017-04-15 23:35:35 +0200

    Egy kis összevonás, semmi komoly

--- ProjlabProto/src/main/Application.java
+++ ProjlabProto/src/main/Application.java
@@ -72,6 +72,31 @@ public class Application {
 		
 		
 	}
+
+	public static abstract class CommandBase {
+		
+		public final String cmdName;
+		
+		public CommandBase (String cmdName) {
+			this.cmdName = cmdName;
+		}
+		
+		public abstract void execute(String[] params);
+		
+		public static class CmdAddRail extends CommandBase{
+
+			public CmdAddRail() {
+				super("add rail");
+			}
+
+			@Override
+			public void execute(String[] params) {
+				System.out.println("yey");
+				
+			}
+		}
+	}
+	
 	
 	private static class CmdSwitchInput extends CommandBase {
 
