ed6a2f4d84da9f3246cf8ba97a80f563dd3fa69a
mmlaj <mmlaj@DESKTOP-J07EC1U>
2017-04-16 22:13:16 +0200

    stationok logikái

--- ProjlabProto/src/main/SimultanStation.java
+++ ProjlabProto/src/main/SimultanStation.java
@@ -4,9 +4,22 @@ public class SimultanStation extends Station{
 	public SimultanStation(Railway railway, Color color){
 		super(railway,color);
 	}
+	@Override
 	public Color getColor(Cart toMe){ 
 		
-		
+		if(toMe.getColor().equals(this.color))
+		{
+			if(!toMe.getPassengers())
+			{
+				toMe.setPassengers(true);
+				return Color.FELSZALLTAK;
+			}
+			else
+			{
+				return Color.HORROR;
+			}
+		}
+		return Color.HORROR;
 		
 		
 		
