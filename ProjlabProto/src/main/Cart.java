d3a169ba9df9541e349130195e9f8fd603c83e0c
Doma <jozsa.domi@gmail.com>
2017-04-16 22:16:17 +0200

    Cart kommentek + setPassengers() függvény

--- ProjlabProto/src/main/Cart.java
+++ ProjlabProto/src/main/Cart.java
@@ -6,14 +6,6 @@ public class Cart extends MovingObject {
 	private boolean Passengers;
 	
 	/**
-
-	 * Konstruktor. A param�ter�l kapott �rt�keket �ll�tja be rendre:
-	 * a s�nt ahol a vagon tart�zkodik, az el�z� tart�zkod�si hely�t a vagonnak,
-	 * a vagont amit h�z, a sz�n�t, �s hogy vannak-e rajta utasok.
-	 * @param railwaySegment S�n, ahol a vagon tart�zkodik
-	 * @param previousRailwaySegment El�z� tart�zkod�si hely
-	 * @param nextCart Vagont amit h�z
-	 * @param color A vagon sz�ne
 	 * Konstruktor. A parameterul kapott ertekeket allitja be rendre:
 	 * a sint ahol a vagon tartozkodik, az elozo tartozkodasi helyet a vagonnak,
 	 * a vagont amit huz, a szinet, es hogy vannak-e rajta utasok.
@@ -21,7 +13,6 @@ public class Cart extends MovingObject {
 	 * @param previousRailwaySegment Elozo tartozkodasi hely
 	 * @param nextCart Vagont amit huz
 	 * @param color A vagon szine
-
 	 * @param passengers Vannak-e rajta utasok
 	 */
 	public Cart(Railway railwaySegment, Railway previousRailwaySegment, Cart nextCart, Color color, boolean passengers) {
@@ -31,34 +22,33 @@ public class Cart extends MovingObject {
 	}
 	
 	/**
-
-	 * Lesz�ll�tja az utasokat a kocsir�l.
-	 * @param station Az �llom�s sz�ne, amin�l lesz�llnak.
 	 * Leszallitja az utasokat a kocsirol.
 	 * @param station Az allomos szine, aminal leszallnak.
-
 	 */
-	private void leaveTheTrain(Station station) {
-		
-		Passengers = false;
-		
+	public void leaveTheTrain(Station station) {
+		this.Passengers = false;	
 	}
 	
 	/**
-
-	 * Ellen�rzi, hogy a lesz�ll�si felt�telek teljes�lnek-e.
-	 * @param station Az �llom�s, ahol a lesz�ll�s t�rt�nik
-	 * @return A lesz�ll�si felt�telek megvannak-e
-
-	 * Ellenorzi, hogy a leszallasi feltetelek teljesulnek-e.
-	 * @param station Az allomos, ahol a leszallas tortenik
-	 * @return A leszallasi feltetelek megvannak-e
-
+	 * Felszallitja az utasokat a vagonra.
+	 */
+	public void setPassengers() {
+		this.Passengers = true;
+	}
+	
+	/**
+	 * Visszaadja a kocsi szinet.
+	 * @return A kocsi szine
 	 */
 	public Color getColor(){
 		return this.color;
 	}
 	
+	/**
+	 * Ellenorzi, hogy a leszallasi feltetelek teljesulnek-e.
+	 * @param station Az allomos, ahol a leszallas tortenik
+	 * @return A leszallasi feltetelek megvannak-e
+	 */
 	public boolean colorCheck(Station station) {
 		Color c = station.getColor();
 		if(c.equals(Color.HORROR))
@@ -94,6 +84,5 @@ public class Cart extends MovingObject {
 			}
 		}
 		return false;
-}
-		
+	}	
 }
