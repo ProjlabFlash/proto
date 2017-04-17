2279ff5771eba98db13f331ba020e45f5ce47d31
Koni <koniskolaja@gmail.com>
2017-04-18 00:18:47 +0200

    Új Color

--- ProjlabProto/src/main/Color.java
+++ ProjlabProto/src/main/Color.java
@@ -1,5 +1,21 @@
 package main;
 
 public enum Color {
-	KEK, ZOLD, SARGA, PIROS, HORROR, FELSZALLTAK
+	BLUE("BLUE"), RED("RED"), GREEN("GREEN"), YELLOW("YELLOW"),
+	TEAL("TEAL"), PURPLE("PURPLE"), GREY("GREY"), ORANGE("ORANGE");
+	
+	private String name;
+	
+	Color(String text) {
+	    this.name = text;
+	  }
+	
+	public static Color fromString(String text) {
+	    for (Color c : Color.values()) {
+	      if (c.name.equalsIgnoreCase(text)) {
+	        return c;
+	      }
+	    }
+	    return null;
+	  }
 }
