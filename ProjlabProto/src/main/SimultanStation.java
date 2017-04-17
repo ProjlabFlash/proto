450a47dab7eca4d3cf4ff133ee96492e6be15270
Doma <jozsa.domi@gmail.com>
2017-04-17 21:00:27 +0200

    SimultanStation kommentek kész

--- ProjlabProto/src/main/SimultanStation.java
+++ ProjlabProto/src/main/SimultanStation.java
@@ -1,9 +1,24 @@
 package main;
 
 public class SimultanStation extends Station{
+	
+	/**
+	 * Konstruktor. Parameterul kapja, hogy az allomas melyik sinnel all. Ezen meghivja a setStation(Station)
+	 * fuggvenyt a sajat referenciajaval. A masodik parameter az allomas szine.
+	 * @param railway Amelyik sinnel all
+	 * @param color Az allomas szine
+	 */
 	public SimultanStation(Railway railway, Color color){
 		super(railway,color);
 	}
+	
+	/**
+	 * Jelzoszinnel ter vissza, jelezve, hogy tortent-e felszallas. Ha megteheto, lebonyolit egy felszallast, ugy,
+	 * hogy lekerdezi a parameterul kapott vagon szinet, majd a Passengers attributumat, es ha azonos szinu, es ures
+	 * a vagon, akkor beallitja true-ra a Passengers attributumot.
+	 * @param toMe A parameterul kapott kocsi
+	 * @return A kocsi szine
+	 */
 	@Override
 	public Color getColor(Cart toMe){ 
 		
