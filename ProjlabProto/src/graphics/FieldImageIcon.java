23656cf227cda4132c35405c1ab3e68555ae7f43
mmlaj <mmlaj@DESKTOP-J07EC1U>
2017-05-09 19:29:19 +0200

    fölös ctor out

--- ProjlabProto/src/graphics/FieldImageIcon.java
+++ ProjlabProto/src/graphics/FieldImageIcon.java
@@ -24,11 +24,7 @@ public class FieldImageIcon extends ImageIcon{
 	private HashMap<String, Image> SwitchImages = new HashMap<String, Image>();
 
 	
-	public FieldImageIcon(){
-		//2do wtf happened several times
-		SwitchImages = new HashMap<String, Image>();
-		
-	}
+	
 	
 	private File getAbsolutePath(String relativePath) {
 		File file = new File(System.getProperty("user.dir"));
